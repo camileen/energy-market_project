0ffedc42d772b709bd9d6aba4a5e5a93dbcb28e9
Xinyi25 <xinyi.zhao1225@gmail.com>
2023-01-29 22:44:49 +0100

    seperate file sigal sending

--- external.py
+++ external.py
@@ -1,17 +1,78 @@
+# import os
+# import signal
+# import subprocess
+
+# def handle_sigusr1(signum, frame):
+#     print("Received SIGUSR1, returning int")
+#     return int
+
+# def external_process():
+#     input_str = input("Enter a string: ")
+#     if input_str == "zxy":
+#         os.kill(os.getppid(), signal.SIGUSR1)
+
+# if __name__ == "__main__":
+#     signal.signal(signal.SIGUSR1, handle_sigusr1)
+#     proc = subprocess.Popen(["python", "-c", "import external; external.external_process()"])
+#     signal.pause()
+
+# import signal
+# import time
+
+# def signal_handler(signum, frame):
+#     print("Signal Number:", signum, "Frame: ", frame)
+
+# def exit_handler(signum, frame):
+#     print("Exiting...")
+#     exit(0)
+
+# signal.signal(signal.SIGINT, signal_handler)
+
+# signal.signal(signal.SIGTSTP, exit_handler)
+
+# while 1:
+#     print("Enter something.")
+#     time.sleep(3)
+# import signal
+# import os
+# import sys
+# import termios
+# import tty
+
+# def handle_sigusr1(signum, frame):
+#     print("Received SIGUSR1, returning int")
+#     return int
+
+# def getch():
+#     fd = sys.stdin.fileno()
+#     old_settings = termios.tcgetattr(fd)
+#     try:
+#         tty.setraw(fd)
+#         ch = sys.stdin.read(1)
+#     finally:
+#         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+#     return ch
+
+# if __name__ == "__main__":
+#     signal.signal(signal.SIGUSR1, handle_sigusr1)
+#     while True:
+#         ch = getch()
+#         if ch == '\x01': # Ctrl+A
+#             os.kill(os.getpid(), signal.SIGUSR1)
+
 import os
 import signal
-import subprocess
+import market
 
-def handle_sigusr1(signum, frame):
-    print("Received SIGUSR1, returning int")
-    return int
+print("market process id:", market.pid())
 
-def external_process():
-    input_str = input("Enter a string: ")
-    if input_str == "zxy":
-        os.kill(os.getppid(), signal.SIGUSR1)
+pid = int(input("Enter the PID of the receiver process: "))
 
-if __name__ == "__main__":
-    signal.signal(signal.SIGUSR1, handle_sigusr1)
-    proc = subprocess.Popen(["python", "-c", "import example; example.external_process()"])
-    signal.pause()
+os.kill(pid, signal.SIGUSR1)
+print("Signal sent!")
+
+            
+
+
+
+
--- market.py
+++ market.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import random
 import time
 import os
@@ -8,9 +7,13 @@ import season
 import struct
 import threading
 import multiprocessing
+import signal
 from threading import Thread
 from multiprocessing import Process, Lock
 import socket
+import termios
+import tty
+#import external
 
 season_list = ["Spring", "Summer", "Automn", "Winter"]
 mutex = Lock()
@@ -22,7 +25,7 @@ E_CONSTANT = 0.01    # source
 S_COMSTANT = 0.1     # season
 
 HOST = "localhost"
-PORT = 23333
+PORT = 23334
 
 
 # ---------------------------- Market ---------------------------------------------------------
@@ -34,12 +37,20 @@ class Market:
         self.season_change = season_change
         self.market_change_return = market_change_return
 
+        print("Market Process id:" , os.getpid())
         self.show_season = Process(target=self.get_season)
         self.show_season.start()
         self.show_weather = Thread(target=self.get_weather)
         self.show_weather.start()
         self.socket = Thread(target=self.handle_socket)
         self.socket.start()
+
+        signal.signal(signal.SIGUSR1, self.signal_handler)
+        print("Market Process id:" , os.getpid())
+        # self.external_process = Process(target=self.recieve_signal)
+        # self.external_process.start() 
+        signal.pause()
+
 
     def get_season(self): 
         i=0
@@ -59,15 +70,14 @@ class Market:
                 try:
                     self.temperature_flag.value = 0
                     print("[Temperature, Rain]: ",self.meteo_shared[:])
+                    self.get_price()
                 finally:
                     mutex.release()
 
     def get_price(self):
         price = 0.145
-        mutex.acquire()
         price = 0.99*price + 0.001*(self.meteo_shared[0]-3*self.meteo_shared[1])
         print("instant price:", price)
-        mutex.release()
         return price
 
     def handle_socket(self):
@@ -87,16 +97,32 @@ class Market:
                         client_socket.send(data_send)
                         data = client_socket.recv(1024)
                     print("Disconnecting from client: ", address)
+
+    def recieve_signal(self):
+        while True:
+            print("enter something:")
+            ch = self.getch()
+            if ch == '\x01': # Ctrl+A
+                print("got it")
+                os.kill(os.getpid(), signal.SIGUSR1)
+                time.sleep(2)
+
+    def getch(self):
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+    def signal_handler(self,signum, frame):
+        print("Signal recieve!!!")
+
     
             
     #def run(self):
-        
-        
-
-        
-        
-        
-
         # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         #     server_socket.setblocking(False)
         #     server_socket.bind((HOST, PORT))
@@ -109,113 +135,4 @@ class Market:
         #             p.start()
     
 
-=======
-import random
-import time
-import os
-import sys
-import weather
-import season
-import struct
-import threading
-import multiprocessing
-from threading import Thread
-from multiprocessing import Process, Lock
-import socket
-
-season_list = ["Spring", "Summer", "Automn", "Winter"]
-mutex = Lock()
-access_price = Lock()
-
-T_CONSTANT = 0.001   # temperature
-R_CONSTANT = 0.002   # rain
-E_CONSTANT = 0.01    # source
-S_COMSTANT = 0.1     # season
-
-HOST = "localhost"
-PORT = 6666
-
-
-# ---------------------------- Market ---------------------------------------------------------
-class Market:
-    def __init__(self,meteo_shared,temperature_flag, meteo,season_change, market_change_return ):
-        self.meteo_shared = meteo_shared
-        self.temperature_flag = temperature_flag
-        self.meteo = meteo
-        self.season_change = season_change
-        self.market_change_return = market_change_return
-
-    def get_season(self): 
-        i=0
-        while True:
-            self.market_change_return.wait()
-            turn = i % 4
-            i += 1
-            print("----- Now we are in", season_list[turn],"-----")
-            self.market_change_return.clear()
-
-    def get_weather(self):  
-        #print("get_weather in process:", os.getpid(), "child de:", os.getppid())
-        while True:
-            while self.temperature_flag.value == 1:
-                #print(weather_flag.value)
-                mutex.acquire()
-                try:
-                    self.temperature_flag.value = 0
-                    print("[Temperature, Rain]: ",self.meteo_shared[:])
-                finally:
-                    mutex.release()
-
-    def get_price(self):
-        price = 0.145
-        mutex.acquire()
-        price = 0.99*price + 0.001*(1/self.meteo_shared[0]-1/self.meteo_shared[1])
-        print("instant price:", price)
-        mutex.release()
-        return price
-
-    # def handle_socket(self):
-    #     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-    #         server_socket.bind((HOST, PORT))
-    #         server_socket.listen()
-    #         client_socket, address = server_socket.accept()
-    #         with client_socket:
-    #             print("Connected to client: ", address)
-    #             data = client_socket.recv(1024)
-    #             while len(data):
-    #                 message = self.get_price()
-    #                 client_socket.sendall(message.encode())
-    #                 data = client_socket.recv(1024)
-    #             print("Disconnecting from client: ", address)
-    
-            
-    def run(self):
-        
-        self.show_season = Thread(target=self.get_season)
-        self.show_season.start()
-        self.show_weather = Thread(target=self.get_weather)
-        self.show_weather.start()
-
-        while True:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-                server_socket.bind((HOST, PORT))
-                server_socket.listen()
-                client_socket, address = server_socket.accept()
-                with client_socket:
-                    print("Connected to client: ", address)
-                    data = client_socket.recv(1024)
-                    while len(data):
-                        price = round(self.get_price(), 4)
-                        message = [price,0]
-                        data_send = struct.pack('<2f',*message)
-                        client_socket.send(data_send)
-                        data = client_socket.recv(1024)
-                    print("Disconnecting from client: ", address)
-        
-        
-
-        
-    
-
->>>>>>> 762d40c72f6f9dd5625ac3ba4081747edae52061
     
